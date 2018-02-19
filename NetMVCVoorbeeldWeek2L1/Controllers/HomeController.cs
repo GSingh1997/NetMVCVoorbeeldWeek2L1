bd6719433dbc9c52a9e6cde99b34347aef05c59a
GurinderSingh01 <gurinderbelgium997@gmail.com>
2018-02-19 11:29:12 +0100

    HomeController

---
+++ NetMVCVoorbeeldWeek2L1/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetMVCVoorbeeldWeek2L1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index(string name)
+        {
+            return $"Hallo {name}";
+        }
+
+    }
+}
